60181b6ddf97f15067b5a66b1e0657327ebdf34b
TheDarkLordMelkor <p.purkarthofer@hakweiz.at>
2017-01-26 09:43:30 +0100

    Spielfeld ready

--- RPSSL/RPSSL/Form1.Designer.cs
+++ RPSSL/RPSSL/Form1.Designer.cs
@@ -28,33 +28,202 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pbx_melkorpb = new System.Windows.Forms.PictureBox();
+            this.pbx_minunpb = new System.Windows.Forms.PictureBox();
+            this.pbx_robot = new System.Windows.Forms.PictureBox();
+            this.pbx_pirate = new System.Windows.Forms.PictureBox();
+            this.pbx_ninja = new System.Windows.Forms.PictureBox();
+            this.pbx_monkey = new System.Windows.Forms.PictureBox();
+            this.pbx_zombie = new System.Windows.Forms.PictureBox();
+            this.pbx_minun = new System.Windows.Forms.PictureBox();
+            this.pbx_melkor = new System.Windows.Forms.PictureBox();
+            this.btn_confirm = new System.Windows.Forms.Button();
+            this.btn_instructions = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_melkorpb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_minunpb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_robot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_pirate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_ninja)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_monkey)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_zombie)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_minun)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_melkor)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // pbx_melkorpb
             // 
-            this.button1.Location = new System.Drawing.Point(100, 124);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.pbx_melkorpb.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbx_melkorpb.BackgroundImage")));
+            this.pbx_melkorpb.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbx_melkorpb.Location = new System.Drawing.Point(361, 372);
+            this.pbx_melkorpb.Name = "pbx_melkorpb";
+            this.pbx_melkorpb.Size = new System.Drawing.Size(266, 191);
+            this.pbx_melkorpb.TabIndex = 0;
+            this.pbx_melkorpb.TabStop = false;
+            // 
+            // pbx_minunpb
+            // 
+            this.pbx_minunpb.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbx_minunpb.BackgroundImage")));
+            this.pbx_minunpb.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbx_minunpb.Location = new System.Drawing.Point(361, 12);
+            this.pbx_minunpb.Name = "pbx_minunpb";
+            this.pbx_minunpb.Size = new System.Drawing.Size(266, 191);
+            this.pbx_minunpb.TabIndex = 1;
+            this.pbx_minunpb.TabStop = false;
+            // 
+            // pbx_robot
+            // 
+            this.pbx_robot.BackColor = System.Drawing.Color.Transparent;
+            this.pbx_robot.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbx_robot.BackgroundImage")));
+            this.pbx_robot.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbx_robot.Location = new System.Drawing.Point(813, 40);
+            this.pbx_robot.Name = "pbx_robot";
+            this.pbx_robot.Size = new System.Drawing.Size(169, 150);
+            this.pbx_robot.TabIndex = 2;
+            this.pbx_robot.TabStop = false;
+            // 
+            // pbx_pirate
+            // 
+            this.pbx_pirate.BackColor = System.Drawing.Color.Transparent;
+            this.pbx_pirate.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbx_pirate.BackgroundImage")));
+            this.pbx_pirate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbx_pirate.Location = new System.Drawing.Point(813, 196);
+            this.pbx_pirate.Name = "pbx_pirate";
+            this.pbx_pirate.Size = new System.Drawing.Size(169, 150);
+            this.pbx_pirate.TabIndex = 3;
+            this.pbx_pirate.TabStop = false;
+            // 
+            // pbx_ninja
+            // 
+            this.pbx_ninja.BackColor = System.Drawing.Color.Transparent;
+            this.pbx_ninja.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbx_ninja.BackgroundImage")));
+            this.pbx_ninja.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbx_ninja.Location = new System.Drawing.Point(813, 352);
+            this.pbx_ninja.Name = "pbx_ninja";
+            this.pbx_ninja.Size = new System.Drawing.Size(169, 150);
+            this.pbx_ninja.TabIndex = 4;
+            this.pbx_ninja.TabStop = false;
+            // 
+            // pbx_monkey
+            // 
+            this.pbx_monkey.BackColor = System.Drawing.Color.Transparent;
+            this.pbx_monkey.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbx_monkey.BackgroundImage")));
+            this.pbx_monkey.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbx_monkey.Location = new System.Drawing.Point(12, 363);
+            this.pbx_monkey.Name = "pbx_monkey";
+            this.pbx_monkey.Size = new System.Drawing.Size(169, 150);
+            this.pbx_monkey.TabIndex = 5;
+            this.pbx_monkey.TabStop = false;
+            // 
+            // pbx_zombie
+            // 
+            this.pbx_zombie.BackColor = System.Drawing.Color.Transparent;
+            this.pbx_zombie.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbx_zombie.BackgroundImage")));
+            this.pbx_zombie.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbx_zombie.Location = new System.Drawing.Point(12, 196);
+            this.pbx_zombie.Name = "pbx_zombie";
+            this.pbx_zombie.Size = new System.Drawing.Size(169, 150);
+            this.pbx_zombie.TabIndex = 6;
+            this.pbx_zombie.TabStop = false;
+            // 
+            // pbx_minun
+            // 
+            this.pbx_minun.BackColor = System.Drawing.Color.Transparent;
+            this.pbx_minun.Location = new System.Drawing.Point(361, 209);
+            this.pbx_minun.Name = "pbx_minun";
+            this.pbx_minun.Size = new System.Drawing.Size(119, 109);
+            this.pbx_minun.TabIndex = 7;
+            this.pbx_minun.TabStop = false;
+            // 
+            // pbx_melkor
+            // 
+            this.pbx_melkor.BackColor = System.Drawing.Color.Transparent;
+            this.pbx_melkor.Location = new System.Drawing.Point(508, 257);
+            this.pbx_melkor.Name = "pbx_melkor";
+            this.pbx_melkor.Size = new System.Drawing.Size(119, 109);
+            this.pbx_melkor.TabIndex = 8;
+            this.pbx_melkor.TabStop = false;
+            // 
+            // btn_confirm
+            // 
+            this.btn_confirm.Location = new System.Drawing.Point(813, 524);
+            this.btn_confirm.Name = "btn_confirm";
+            this.btn_confirm.Size = new System.Drawing.Size(169, 39);
+            this.btn_confirm.TabIndex = 9;
+            this.btn_confirm.Text = "Auswahl bestätigen";
+            this.btn_confirm.UseVisualStyleBackColor = true;
+            // 
+            // btn_instructions
+            // 
+            this.btn_instructions.Location = new System.Drawing.Point(12, 12);
+            this.btn_instructions.Name = "btn_instructions";
+            this.btn_instructions.Size = new System.Drawing.Size(169, 39);
+            this.btn_instructions.TabIndex = 10;
+            this.btn_instructions.Text = "Spielanleitung";
+            this.btn_instructions.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(478, 268);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "VS.";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(775, 492);
-            this.Controls.Add(this.button1);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(994, 575);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_instructions);
+            this.Controls.Add(this.btn_confirm);
+            this.Controls.Add(this.pbx_melkor);
+            this.Controls.Add(this.pbx_minun);
+            this.Controls.Add(this.pbx_zombie);
+            this.Controls.Add(this.pbx_monkey);
+            this.Controls.Add(this.pbx_ninja);
+            this.Controls.Add(this.pbx_pirate);
+            this.Controls.Add(this.pbx_robot);
+            this.Controls.Add(this.pbx_minunpb);
+            this.Controls.Add(this.pbx_melkorpb);
+            this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_melkorpb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_minunpb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_robot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_pirate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_ninja)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_monkey)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_zombie)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_minun)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_melkor)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pbx_melkorpb;
+        private System.Windows.Forms.PictureBox pbx_minunpb;
+        private System.Windows.Forms.PictureBox pbx_robot;
+        private System.Windows.Forms.PictureBox pbx_pirate;
+        private System.Windows.Forms.PictureBox pbx_ninja;
+        private System.Windows.Forms.PictureBox pbx_monkey;
+        private System.Windows.Forms.PictureBox pbx_zombie;
+        private System.Windows.Forms.PictureBox pbx_minun;
+        private System.Windows.Forms.PictureBox pbx_melkor;
+        private System.Windows.Forms.Button btn_confirm;
+        private System.Windows.Forms.Button btn_instructions;
+        private System.Windows.Forms.Label label1;
     }
 }
 
