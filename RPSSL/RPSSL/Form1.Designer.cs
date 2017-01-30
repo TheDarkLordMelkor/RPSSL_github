a3bf99b40a56b54ae69ffcff611cc85605c3b2ca
TheDarkLordMelkor <p.purkarthofer@hakweiz.at>
2017-01-30 08:31:51 +0100

    Spielfeld aktu

--- RPSSL/RPSSL/Form1.Designer.cs
+++ RPSSL/RPSSL/Form1.Designer.cs
@@ -41,6 +41,11 @@
             this.btn_confirm = new System.Windows.Forms.Button();
             this.btn_instructions = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pbx_melkorpb)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbx_minunpb)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbx_robot)).BeginInit();
@@ -82,6 +87,7 @@
             this.pbx_robot.Size = new System.Drawing.Size(169, 150);
             this.pbx_robot.TabIndex = 2;
             this.pbx_robot.TabStop = false;
+            this.pbx_robot.Click += new System.EventHandler(this.pbx_robot_Click);
             // 
             // pbx_pirate
             // 
@@ -93,6 +99,7 @@
             this.pbx_pirate.Size = new System.Drawing.Size(169, 150);
             this.pbx_pirate.TabIndex = 3;
             this.pbx_pirate.TabStop = false;
+            this.pbx_pirate.Click += new System.EventHandler(this.pbx_pirate_Click);
             // 
             // pbx_ninja
             // 
@@ -104,6 +111,7 @@
             this.pbx_ninja.Size = new System.Drawing.Size(169, 150);
             this.pbx_ninja.TabIndex = 4;
             this.pbx_ninja.TabStop = false;
+            this.pbx_ninja.Click += new System.EventHandler(this.pbx_ninja_Click);
             // 
             // pbx_monkey
             // 
@@ -115,6 +123,7 @@
             this.pbx_monkey.Size = new System.Drawing.Size(169, 150);
             this.pbx_monkey.TabIndex = 5;
             this.pbx_monkey.TabStop = false;
+            this.pbx_monkey.Click += new System.EventHandler(this.pbx_monkey_Click);
             // 
             // pbx_zombie
             // 
@@ -126,6 +135,7 @@
             this.pbx_zombie.Size = new System.Drawing.Size(169, 150);
             this.pbx_zombie.TabIndex = 6;
             this.pbx_zombie.TabStop = false;
+            this.pbx_zombie.Click += new System.EventHandler(this.pbx_zombie_Click);
             // 
             // pbx_minun
             // 
@@ -135,6 +145,7 @@
             this.pbx_minun.Size = new System.Drawing.Size(119, 109);
             this.pbx_minun.TabIndex = 7;
             this.pbx_minun.TabStop = false;
+            this.pbx_minun.Click += new System.EventHandler(this.pbx_minun_Click);
             // 
             // pbx_melkor
             // 
@@ -144,24 +155,33 @@
             this.pbx_melkor.Size = new System.Drawing.Size(119, 109);
             this.pbx_melkor.TabIndex = 8;
             this.pbx_melkor.TabStop = false;
+            this.pbx_melkor.Click += new System.EventHandler(this.pbx_melkor_Click);
             // 
             // btn_confirm
             // 
+            this.btn_confirm.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_confirm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_confirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_confirm.Location = new System.Drawing.Point(813, 524);
             this.btn_confirm.Name = "btn_confirm";
             this.btn_confirm.Size = new System.Drawing.Size(169, 39);
             this.btn_confirm.TabIndex = 9;
             this.btn_confirm.Text = "Auswahl bestätigen";
-            this.btn_confirm.UseVisualStyleBackColor = true;
+            this.btn_confirm.UseVisualStyleBackColor = false;
+            this.btn_confirm.Click += new System.EventHandler(this.btn_confirm_Click);
             // 
             // btn_instructions
             // 
+            this.btn_instructions.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btn_instructions.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_instructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_instructions.Location = new System.Drawing.Point(12, 12);
             this.btn_instructions.Name = "btn_instructions";
             this.btn_instructions.Size = new System.Drawing.Size(169, 39);
             this.btn_instructions.TabIndex = 10;
             this.btn_instructions.Text = "Spielanleitung";
-            this.btn_instructions.UseVisualStyleBackColor = true;
+            this.btn_instructions.UseVisualStyleBackColor = false;
+            this.btn_instructions.Click += new System.EventHandler(this.btn_instructions_Click);
             // 
             // label1
             // 
@@ -174,13 +194,74 @@
             this.label1.TabIndex = 11;
             this.label1.Text = "VS.";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(187, 268);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Zombie";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(187, 427);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Monkey";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(756, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 17);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Robot";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(756, 268);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 17);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Pirate";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(762, 427);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 17);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Ninja";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(994, 575);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_instructions);
             this.Controls.Add(this.btn_confirm);
@@ -224,6 +305,11 @@
         private System.Windows.Forms.Button btn_confirm;
         private System.Windows.Forms.Button btn_instructions;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- RPSSL/RPSSL/Form1.cs
+++ RPSSL/RPSSL/Form1.cs
@@ -17,5 +17,50 @@ namespace RPSSL
         {
             InitializeComponent();
         }
+
+        private void pbx_robot_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbx_pirate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbx_ninja_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbx_monkey_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbx_zombie_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_instructions_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Robot: schlägt Zombie und Ninja \nPirate: schlägt Robot und Monkey \nNinja: schlägt Pirate und Zombie \nMonkey: schlägt Ninja und Robot \nZombie: schlägt Monkey und Pirate");
+        }
+
+        private void btn_confirm_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbx_minun_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pbx_melkor_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
